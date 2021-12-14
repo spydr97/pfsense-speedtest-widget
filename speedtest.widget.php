43a94dabda90192905fd3eed28d3632989b0a52c
Tristen Paul <tristenpaul@protonmail.com>
2021-12-14 04:04:21 +0200

    Update widget to use new CLI JSON format

--- speedtest.widget.php
+++ speedtest.widget.php
@@ -35,53 +35,69 @@ if ($_REQUEST['ajax']) {
         $results = null;
     }
 ?>
-<table class="table">
+<table class="table table-striped">
 	<tr>
-		<td><h4>Ping <i class="fa fa-exchange"></h4></td>
-		<td><h4>Download <i class="fa fa-download"></i></h4></td>
-		<td><h4>Upload <i class="fa fa-upload"></h4></td>
+		<td><h4>Ping</h4></td>
+		<td><h4>Jitter</h4></td>
+		<td><h4>Download</h4></td>
+		<td><h4>Upload</h4></td>
 	</tr>
 	<tr>
-		<td><h4 id="speedtest-ping">N/A</h4></td>
+		<td><h4 id="speedtest-ping-latency">N/A</h4></td>
+		<td><h4 id="speedtest-ping-jitter">N/A</h4></td>
 		<td><h4 id="speedtest-download">N/A</h4></td>
 		<td><h4 id="speedtest-upload">N/A</h4></td>
 	</tr>
-	<tr>
+		<td>Server</td>
+		<td id="speedtest-server-name">N/A</td>
+		<td id="speedtest-server-ip">N/A</td>
+		<td id="speedtest-server-location">N/A</td>
+	</tr>
 		<td>ISP</td>
-		<td colspan="2" id="speedtest-isp">N/A</td>
+		<td id="speedtest-isp">N/A</td>
+		<td id="speedtest-external-ip">N/A</td>
+		<td>-</td>
 	</tr>
 	<tr>
-		<td>Host</td>
-		<td colspan="2" id="speedtest-host">N/A</td>
+        <td>Date</td>
+		<td colspan="3" id="speedtest-timestamp">&nbsp;</td>
 	</tr>
 	<tr>
-		<td colspan="3" id="speedtest-ts" style="font-size: 0.8em;">&nbsp;</td>
+        <td>Result</td>
+		<td colspan="3" id="speedtest-result-id">&nbsp;</td>
 	</tr>
 </table>
 <a id="updspeed" href="#" class="fa fa-refresh" style="display: none;"></a>
 <script type="text/javascript">
-function update_result(results) {
+function updateResult(results) {
     if(results != null) {
-    	var date = new Date(results.timestamp);
-    	$("#speedtest-ts").html(date);
-    	$("#speedtest-ping").html(results.ping.toFixed(2) + "<small> ms</small>");
-    	$("#speedtest-download").html((results.download / 1000000).toFixed(2) + "<small> Mbps</small>");
-    	$("#speedtest-upload").html((results.upload / 1000000).toFixed(2) + "<small> Mbps</small>");
-    	$("#speedtest-upload").html((results.upload / 1000000).toFixed(2) + "<small> Mbps</small>");
-    	$("#speedtest-isp").html(results.client.isp);
-    	$("#speedtest-host").html(results.server.name);
+    	$("#speedtest-timestamp").html(new Date(results.timestamp));
+        $("#speedtest-ping-jitter").html(results.ping.jitter.toFixed(4) + "<small> ms</small>");
+    	$("#speedtest-ping-latency").html(results.ping.latency.toFixed(4) + "<small> ms</small>");
+    	$("#speedtest-download").html((results.download.bandwidth / 100000).toFixed(4) + "<small> Mbps</small>");
+    	$("#speedtest-upload").html((results.upload.bandwidth / 100000).toFixed(4) + "<small> Mbps</small>");
+    	$("#speedtest-isp").html(results.isp);
+    	$("#speedtest-external-ip").html(results.interface.externalIp);
+    	$("#speedtest-server-name").html(results.server.name);
+    	$("#speedtest-server-location").html(results.server.location);
+    	$("#speedtest-server-ip").html(results.server.ip);
+    	$("#speedtest-result-id").html(results.result.id);
     } else {
     	$("#speedtest-ts").html("Speedtest failed");
-    	$("#speedtest-ping").html("N/A");
+        $("#speedtest-ping-jitter").html("N/A");
+    	$("#speedtest-ping-latency").html("N/A");
     	$("#speedtest-download").html("N/A");
     	$("#speedtest-upload").html("N/A");
-    	$("#speedtest-upload").html("N/A");
     	$("#speedtest-isp").html("N/A");
-    	$("#speedtest-host").html("N/A");
+    	$("#speedtest-external-ip").html("N/A");
+    	$("#speedtest-server-name").html("N/A");
+    	$("#speedtest-server-location").html("N/A");
+    	$("#speedtest-server-ip").html("N/A");
+    	$("#speedtest-result-id").html("N/A");
     }
 }
 
-function update_speedtest() {
+function updateSpeedtest() {
     $('#updspeed').off("click").blur().addClass("fa-spin").click(function() {
         $('#updspeed').blur();
         return false;
@@ -94,14 +110,14 @@ function update_speedtest() {
             ajax: "ajax"
         },
         success: function(data) {
-            update_result(data);
+            updateResult(data);
         },
         error: function() {
-            update_result(null);
+            updateResult(null);
         },
         complete: function() {
             $('#updspeed').off("click").removeClass("fa-spin").click(function() {
-                update_speedtest();
+                updateSpeedtest();
                 return false;
             });
         }
@@ -111,10 +127,10 @@ events.push(function() {
 	var target = $("#updspeed").closest(".panel").find(".widget-heading-icon");
 	$("#updspeed").prependTo(target).show();
     $('#updspeed').click(function() {
-        update_speedtest();
+        updateSpeedtest();
         return false;
     });
-    update_result(<?php echo ($results === null ? "null" : $results); ?>);
+    updateResult(<?php echo ($results === null ? "null" : $results); ?>);
 });
 </script>
 <?php } ?>
